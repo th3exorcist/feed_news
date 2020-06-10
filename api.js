e2143b6831eff178c0898625adece88d7140992d
th3exorcist <ghostsavestheworld@gmail.com>
2020-06-10 14:31:19 -0300

    feednews clone

---
+++ api.js
@@ -0,0 +1,23 @@
+const graphqlFetch = require('graphql-fetch') ('https://www.graphqlhub.com/graphql')
+
+const graphqlQuery = (category) => `
+{
+  hn {
+    ${category} Stories (limit: 50) {
+      title
+      url
+      timeISO
+      by {
+        id
+      }
+    }
+  }
+}
+`
+
+const fetchStories = async (category) => {
+  const res = await graphqlFetch(graphqlQuery(category))
+  return {stories: res.data.hn [`${category}Stories`]}
+}
+
+export {fetchStories}
---
+++ pages/index.js
@@ -0,0 +1,84 @@
+import React from 'react'
+import css from 'next/css'
+import Head from 'next/head'
+import Link from 'next/link'
+import moment from  'moment'
+import {fetchStories} from '../api'
+
+const categories = ['show', 'job', 'ask']
+
+const Nav = () => (
+ <nav className={navStyle}>
+    <ul>
+      {categories.map(cat => (
+        <li key={cat}>
+          <Link href={`/?category=${cat}`}>
+            <a>{cat}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </nav>
+)
+
+const Story = ({story}) => (
+   <div>
+    <h4><a href={story.url}>{story.title}</a></h4>
+    <span>{story.score} Points - 
+      Posted {moment(story.timeISO).fromNow()} 
+      by {story.by.id}
+    </span>
+  </div>
+)
+
+const Stories = ({stories}) => (
+  <ol>
+    {stories.map((story, index) => (
+      <li key={index}>
+        <Story story={story} />
+      </li>
+    ))}
+  </ol>
+)
+
+class App extends React.Component {
+  static async getInitialProps ({query: {category = 'show'}}) {
+  return fetchStories(category)
+}
+
+render () {
+
+    const {stories} = this.props
+    return (
+      <div className={appStyle}>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
+        <Nav />
+        <Stories stories={this.props.stories} />
+      </div>
+   )
+
+}
+}
+
+const appStyle = css({
+  fontFamily: 'Verdana, Geneva, sans-serif',
+  color: '#333',
+  margin: '0 auto',
+  maxWidth: 800,
+  '& a': {
+    textDecoration: 'none'
+  }
+})
+
+const navStyle = css({
+  '& li': {
+    display: 'inline-block',
+    textTransform: 'uppercase',
+    margin: '2em 3em 2em 0em',
+    fontWeight: 'bolder'
+  }
+})
+
+export default App
